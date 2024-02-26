7361ea7890747390c670ec3121c08558fb62f0ea
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-02-26 23:42:39 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ output "id" {
 }
 resource "azurerm_container_registry" "acr" {
   name                  = "AcrTestlabs"
-  resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name   = var.rg
   location              = azurerm_resource_group.rg.location
   sku                   = "Basic"
   data_endpoint_enabled = false
