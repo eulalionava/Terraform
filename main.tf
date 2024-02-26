c9809d9545fbf5973ac83e5af6d4931f78f14d92
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-02-26 21:41:51 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,5 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   sku                   = "Basic"
-  data_endpoint_enabled = true
+  data_endpoint_enabled = false
 }
