3fc87c939db492da046f764c594034aee3143d38
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-02-26 19:27:34 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,7 @@
 terraform {
-
-  # Configuración del backend
   backend "azurerm" {
     storage_account_name = "storageterraform3"
+    resource_group_name  = "RG-TESTLABS-RM"
     container_name       = "terraformcontainer"
-    key                  = "terraform.tfstate"
   }
-
-  # Opciones adicionales de configuración
-  # Aquí puedes agregar otras configuraciones de Terraform según sea necesario
 }
