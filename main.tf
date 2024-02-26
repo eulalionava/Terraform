b1f28a7a2b9bdb3006b129f95de20a8827193330
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-02-26 18:49:24 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 resource "azurerm_container_registry" "acr" {
-  name                  = "acr-testlabs"
+  name                  = "AcrTestlabs"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   sku                   = "basic"
