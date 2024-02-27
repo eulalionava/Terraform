681881af7818e781e3630333f97db555fb3a0ae9
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-02-27 22:09:08 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -44,14 +44,14 @@ resource "azurerm_key_vault" "kv" {
   location                    = var.location
   resource_group_name         = var.rg
   enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  tenant_id                   = var.tenant
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   sku_name = "standard"
 
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
+    tenant_id = var.tenant
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
