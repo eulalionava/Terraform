6154233934ab464aaebaedc03b336d5829df6ce4
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-02-27 22:24:04 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -20,21 +20,36 @@ resource "azurerm_container_registry" "acr" {
   }
 }
 
-resource "azurerm_arc_kubernetes_cluster" "aks" {
-  name                         = "AksTestlabs"
-  resource_group_name          = var.rg
-  location                     = var.location
-  
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "AksTestlabs"
+  location            = var.location
+  resource_group_name = var.rg
+#  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
   identity {
     type = "SystemAssigned"
   }
 
   tags = {
-    Environment = "Develop"
-    Department  = "EH"
-    Createdby   = "Terraform"
-    EmailOwner  = "acardenas@readymind.ms"
+    Environment = "Production"
   }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
 }
 
 #data "azurerm_client_config" "current" {}
