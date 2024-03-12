ab2c0d0e684b378681c0408bd19fddaf4a7575d6
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-12 23:01:13 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -129,7 +129,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
   identity {
     type = "SystemAssigned"
   }
-
+  role_based_access_control {
+    enabled = true
+  }
+  network_profile {
+    network_plugin = "azure"
+  }
   tags = {
     Environment = "Production"
   }
