95ffcf59b6702c5849e84572d3b874a948809e9e
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-12 22:27:20 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+#Grupo de recursos para las vnet
 resource "azurerm_resource_group" "VNET-RG" {
   name     = "VNET-RG"
   location = var.location
@@ -14,4 +15,74 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.VNET-RG.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+#Grupo de recursos para el application gateway
+resource "azurerm_resource_group" "natus-seg-rg" {
+  name     = "natus-seg-rg"
+  location = var.location
+}
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.vnet.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.vnet.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnet.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.vnet.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.vnet.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.vnet.name}-rdrcfg"
+}
+resource "azurerm_application_gateway" "appgw" {
+  name                = "appgw01"
+  resource_group_name = azurerm_resource_group.natus-seg-rg.name
+  location            = azurerm_resource_group.natus-seg-rg.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.subnet.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  #frontend_ip_configuration {
+  #  name                 = local.frontend_ip_configuration_name
+  #  public_ip_address_id = azurerm_public_ip.example.id
+  #}
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
 }
