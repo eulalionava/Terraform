0ddb0ec7f4464c8f52861d90ff7deadc4e027ca0
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-13 00:47:47 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "public_ip"
   resource_group_name = azurerm_resource_group.natus-seg-rg.name
   location            = azurerm_resource_group.natus-seg-rg.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Standard"
 }
 
 locals {
