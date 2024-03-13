8a83d2f4d1667037f1e6297aa098d8e88ee347a2
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-13 00:25:17 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 #Grupo de recursos para las vnet
-#resource "azurerm_resource_group" "VNET-RG" {
-#  name     = "VNET-RG"
-#  location = var.location
-data "azurerm_resource_group" "VNET-RG" {
-  name = "VNET-RG"
+resource "azurerm_resource_group" "VNET-RG" {
+  name     = "VNET-RG"
+  location = var.location
 }
 
-output "id2" {
-  value = "/subscriptions/e9cdf5ea-1d5c-48c0-b179-7dc7a4973f06/resourceGroups/VNET-RG"
-}
+#data "azurerm_resource_group" "VNET-RG" {
+#  name = "VNET-RG"
+#}
+#
+#output "id2" {
+#  value = "/subscriptions/e9cdf5ea-1d5c-48c0-b179-7dc7a4973f06/resourceGroups/VNET-RG"
+#}
+
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet01"
   address_space       = ["10.0.0.0/16"]
@@ -24,23 +27,24 @@ resource "azurerm_subnet" "subnet" {
 }
 
 #Grupo de recursos para el application gateway
-#resource "azurerm_resource_group" "natus-seg-rg" {
-#  name     = "natus-seg-rg"
-#  location = var.location
-#  tags = {
-#    Environment = "Natus"
-#    Department  = "EH"
-#    Createdby   = "Terraform"
-#    EmailOwner  = "acardenas@readymind.ms"
-#  }
-#}
-data "azurerm_resource_group" "natus-seg-rg" {
-  name = "natus-seg-rg"
+resource "azurerm_resource_group" "natus-seg-rg" {
+  name     = "natus-seg-rg"
+  location = var.location
+  tags = {
+    Environment = "Natus"
+    Department  = "EH"
+    Createdby   = "Terraform"
+    EmailOwner  = "acardenas@readymind.ms"
+  }
 }
 
-output "id3" {
-  value = "/subscriptions/e9cdf5ea-1d5c-48c0-b179-7dc7a4973f06/resourceGroups/natus-seg-rg"
-}
+#data "azurerm_resource_group" "natus-seg-rg" {
+#  name = "natus-seg-rg"
+#}
+#
+#output "id3" {
+#  value = "/subscriptions/e9cdf5ea-1d5c-48c0-b179-7dc7a4973f06/resourceGroups/natus-seg-rg"
+#}
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "public_ip"
@@ -114,24 +118,25 @@ resource "azurerm_application_gateway" "appgw01" {
   }
 }
 #Grupo de recursos para aks
-#resource "azurerm_resource_group" "natus-aks" {
-#  name     = "natus-aks"
-#  location = var.location
-#  tags = {
-#    Environment = "Natus"
-#    Department  = "EH"
-#    Createdby   = "Terraform"
-#    EmailOwner  = "acardenas@readymind.ms"
-#  }
+resource "azurerm_resource_group" "natus-aks" {
+  name     = "natus-aks"
+  location = var.location
+  tags = {
+    Environment = "Natus"
+    Department  = "EH"
+    Createdby   = "Terraform"
+    EmailOwner  = "acardenas@readymind.ms"
+  }
+}
+
+#data "azurerm_resource_group" "natus-aks" {
+#  name = "natus-aks"
+#}
+#
+#output "id4" {
+#  value = "/subscriptions/e9cdf5ea-1d5c-48c0-b179-7dc7a4973f06/resourceGroups/natus-aks"
 #}
 
-data "azurerm_resource_group" "natus-aks" {
-  name = "natus-aks"
-}
-
-output "id4" {
-  value = "/subscriptions/e9cdf5ea-1d5c-48c0-b179-7dc7a4973f06/resourceGroups/natus-aks"
-}
 resource "azurerm_container_registry" "acr01" {
   name                = "containerRegistry01"
   resource_group_name = "natus-aks"
