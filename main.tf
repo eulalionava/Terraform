c399646a2d0e30b72a47af9d44298b93776e42de
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-19 05:51:10 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -58,6 +58,17 @@ resource "azurerm_public_ip" "bas" {
   allocation_method   = "Static"
   sku                 = "Standard"
 }
+resource "azurerm_bastion_host" "bastion" {
+  name                = "natus-bas"
+  location            = azurerm_resource_group.natus-devops-int.location
+  resource_group_name = azurerm_resource_group.natus-devops-int.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.subnet-bastion.id
+    public_ip_address_id = azurerm_public_ip.subnet-bastion.id
+  }
+}
 #Grupo de recursos para el application gateway
 resource "azurerm_resource_group" "natus-seg-rg" {
   name     = "natus-seg-rg"
