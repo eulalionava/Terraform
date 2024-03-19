81c06c255d168df576afb4922b4c31a63715577d
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-19 06:06:48 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -80,6 +80,33 @@ resource "azurerm_network_interface" "network-interface" {
     private_ip_address_allocation = "Dynamic"
   }
 }
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "agent-azuredevops"
+  resource_group_name = azurerm_resource_group.natus-devops-int.name
+  location            = azurerm_resource_group.natus-devops-int.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.network-interface.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
 #Grupo de recursos para el application gateway
 resource "azurerm_resource_group" "natus-seg-rg" {
   name     = "natus-seg-rg"
