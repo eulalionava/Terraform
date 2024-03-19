c3642172e7bcd85214db22434c545e6fd038cd06
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-19 05:53:00 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_bastion_host" "bastion" {
   ip_configuration {
     name                 = "configuration"
     subnet_id            = azurerm_subnet.subnet-bastion.id
-    public_ip_address_id = azurerm_public_ip.subnet-bastion.id
+    public_ip_address_id = azurerm_public_ip.bas.id
   }
 }
 #Grupo de recursos para el application gateway
