bd3baff9e175c03ca1251bb5ab5b60e8ee9078ed
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-19 05:38:57 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,12 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+resource "azurerm_subnet" "subnet-bastion" {
+  name                 = "subnet-bastion01"
+  resource_group_name  = azurerm_resource_group.VNET-RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
 #Grupo de recursos para el application gateway
 resource "azurerm_resource_group" "natus-seg-rg" {
