7da4754eefe4296d0c38bd360b1fccd342b5e6a2
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-19 20:25:33 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -230,6 +230,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = azurerm_resource_group.natus-aks.name
   location            = azurerm_resource_group.natus-aks.location
   sku                 = "Premium"
+  admin_enabled       = true
   public_network_access_enabled = false
 
   tags = {
