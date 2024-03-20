b5cb14c80bc8ae8d693f11fffb32fc11d679f25e
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-20 18:51:44 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -409,3 +409,23 @@ resource "azurerm_mssql_server" "sqlserver" {
   administrator_login          = "4dm1n157r470r"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
+resource "azurerm_mssql_database" "mysql" {
+  name           = "mysqldb-natus"
+  server_id      = azurerm_mssql_server.sqlserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 4
+  read_scale     = true
+  sku_name       = "S0"
+  zone_redundant = true
+  enclave_type   = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  # prevent the possibility of accidental data loss
+  lifecycle {
+    prevent_destroy = true
+  }
+}
