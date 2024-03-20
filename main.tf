51549b83255feed81969d9a9295a3e750295b0a1
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-20 16:30:17 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -324,7 +324,7 @@ resource "azurerm_role_assignment" "acr" {
   principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
 resource "azurerm_role_assignment" "agw" {
-  scope                = azurerm_resource_group.natus-seg-rg.id
+  scope                = azurerm_resource_group.natus-aks.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_kubernetes_cluster.aks.ingress_application_gateway[0].ingress_application_gateway_identity[0].object_id
 }
