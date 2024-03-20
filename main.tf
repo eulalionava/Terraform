45fbf2f7bbe18771e376697643d86988b3666780
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-20 00:51:36 +0000

    link private dns zones

--- main.tf
+++ main.tf
@@ -247,6 +247,12 @@ resource "azurerm_private_dns_zone" "acr" {
   resource_group_name = azurerm_resource_group.natus-aks.name
 }
 
+resource "azurerm_private_dns_zone_virtual_network_link" "acr1" {
+  name                  = "linkdnsacr01"
+  resource_group_name   = azurerm_resource_group.natus-aks.name
+  private_dns_zone_name = azurerm_private_dns_zone.acr.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
 #AKS
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks1"
