720b76e5584866f220776778a2904942868a77a5
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-03-20 14:58:55 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -279,6 +279,13 @@ resource "azurerm_private_endpoint" "acr" {
   }
 }
 #AKS
+resource "azurerm_log_analytics_workspace" "log" {
+  name                = "log-aks01"
+  location            = azurerm_resource_group.natus-aks.location
+  resource_group_name = azurerm_resource_group.natus-aks.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "aks1"
   location            = azurerm_resource_group.natus-aks.location
