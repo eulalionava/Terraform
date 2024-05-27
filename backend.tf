a95fd2886ac0b43e5c2255401e69e25f74e8c326
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-27 17:20:55 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "azurerm" {
     storage_account_name = "storageterraform3"
     resource_group_name  = "RG-TESTLABS-RM"
-    container_name       = "storagedemo"
+    container_name       = "terraformcontainer"
   }
 }
