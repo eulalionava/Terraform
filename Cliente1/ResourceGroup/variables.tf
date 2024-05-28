b3aa5791d4d564d83025d1fb025d6ef69f37bdf5
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-28 21:47:10 +0000

    Updated variables.tf

--- Cliente1/ResourceGroup/variables.tf
+++ Cliente1/ResourceGroup/variables.tf
@@ -2,12 +2,11 @@ variable "client_name" {
     type = string
     description = "Nombre del cliente"
 }
-
 variable "location" {
     type = string
     description = "localizacion de los recursos"
 }
-variable "NumeroCliente"{
+variable "client_number"{
     type = number
     description = "numero de cliente"
 }
