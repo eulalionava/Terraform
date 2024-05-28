1ae61cd2652d260f2b9d3bdef668b991b63d30fc
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-28 20:13:07 +0000

    Deleted variables.tf

--- Cliente1/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "client_name" {
-    type = string
-    description = "Nombre del cliente"
-}
-
-variable "location" {
-    type = string
-    description = "localizacion de los recursos"
-}
