4738c0fb524a40923fa45837a31323c13b04ee24
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-28 21:44:23 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "Cliente1"{
     source = "./Cliente1/ResourceGroup"
     client_name = "NombreCliente1" #coloque el nombre del cliente nuevo
-    location = "eastus" 
+    location = "eastus"
+    number = 1 
 }
 
 #Crear vm para agente
