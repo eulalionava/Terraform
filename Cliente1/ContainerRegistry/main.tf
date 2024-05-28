36a830c23832e86765926fe94afa6fef5e53355b
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-28 23:27:50 +0000

    Updated main.tf

--- Cliente1/ContainerRegistry/main.tf
+++ Cliente1/ContainerRegistry/main.tf
@@ -1,17 +1,8 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "containerRegistry1"
+  name                = "crpefprd${var.location}client_number-001"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  sku                 = "Premium"
-  admin_enabled       = false
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
-  georeplications {
-    location                = "North Europe"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
+  sku                 = "Basic"
+  admin_enabled       = true
+  
 }
