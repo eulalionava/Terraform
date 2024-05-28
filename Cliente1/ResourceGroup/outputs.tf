dfa3902c9da2b70c039f7edb1e7413bc6b10e7e5
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-28 22:18:41 +0000

    Updated outputs.tf

--- Cliente1/ResourceGroup/outputs.tf
+++ Cliente1/ResourceGroup/outputs.tf
@@ -1,4 +1,12 @@
 output "rg_name_out"{
-    value = azurerm_resource_group.example.name
+    value = azurerm_resource_group.rg-hub.name
+
+}
+output "rg_name_out"{
+    value = azurerm_resource_group.rg-pef.name
+
+}
+output "rg_name_out"{
+    value = azurerm_resource_group.rg-aks.name
 
 }
