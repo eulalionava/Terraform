a24842fcf5076dedf40acc6b0d41f874849dded4
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-28 22:01:52 +0000

    Updated main.tf

--- Cliente1/ResourceGroup/main.tf
+++ Cliente1/ResourceGroup/main.tf
@@ -1,5 +1,12 @@
-#AKS RG
+resource "azurerm_resource_group" "rg-hub" {
+  name     = "rsg-pef-hub-prd-${var.location}client_number-001"
+  location = var.location
+}
 resource "azurerm_resource_group" "example" {
+  name     = "rsg-pef-apm-prd-${var.location}client_number-001"
+  location = var.location
+}
+resource "azurerm_resource_group" "rg-aks" {
   name     = "rsg-pef-k8s-prd-${var.location}client_number-001"
   location = var.location
 }
