35bed881463cd9856000cf90998dccdd36637230
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-29 00:07:26 +0000

    Updated outputs.tf

--- Cliente1/ContainerRegistry/outputs.tf
+++ Cliente1/ContainerRegistry/outputs.tf
@@ -1,4 +1,15 @@
-#output "rg_name_out"{
-#    value = azurerm_resource_group.example.name
-#
-#}
+output "acr_id" {
+  value = azurerm_container_registry.acr.id
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value = azurerm_resource_group.rg_hub.idazurerm_container_registry.acr.admin_password
+}
