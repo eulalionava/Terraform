30b729de40f71e016de05d02b90faf582ca7f67b
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-29 01:16:15 +0000

    Updated variables.tf

--- Cliente1/KeyVault/variables.tf
+++ Cliente1/KeyVault/variables.tf
@@ -2,7 +2,7 @@ variable "tenant" {
     description = "The Tenant ID where the Key Vault is located."
     type        = string
 }
-variable "service_principal_object_id" {
+variable "key_vault_manager" {
     description = "The Object ID of the Service Principal to which permissions will be assigned."
     type        = string
 }
