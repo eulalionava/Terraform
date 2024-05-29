cc3c60a8b6a280f5b05f41fe6d13dd35e203078c
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-29 00:28:19 +0000

    Updated main.tf

--- Cliente1/ContainerRegistry/main.tf
+++ Cliente1/ContainerRegistry/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-  name                = "crpefprd${var.location}client_number-001"
+  name                = "crpefprd${var.location}"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Basic"
