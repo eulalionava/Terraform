317dbcdd3aed3948218b8febf5f09071892d93cb
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-29 00:08:29 +0000

    Updated variables.tf

--- Cliente1/ContainerRegistry/variables.tf
+++ Cliente1/ContainerRegistry/variables.tf
@@ -2,8 +2,15 @@ variable "client_name" {
     type = string
     description = "Nombre del cliente"
 }
-
 variable "location" {
     type = string
     description = "localizacion de los recursos"
+}
+variable "client_number" {
+    description = "The client number for the container registry."
+    type        = string
+}
+variable "resource_group_name" {
+    description = "The name of the resource group where the container registry will be created."
+    type        = string
 }
