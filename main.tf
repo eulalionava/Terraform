6d7e8cbf7228f6895e130ea58555ff219ff7e2d9
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-05-29 02:08:13 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,16 @@
-#CLIENTE 1
 locals {
-  client_name   = "NombreCliente1" #Agregue el nombre del cliente
-  client_number = 1                #Agregue el numero de cliente
   location      = "eastus"         #Agregue la region
   tenant        = "08c7a78d-587d-4487-962e-93c5fb54c7bf" #Agregue el tenant
   key_vault_manager = "7d7c8ee7-f410-4f01-9c33-3744f87ff4e8"  #objectId del administrador de KV
 }
 module "Cliente1_RG"{
-    source = "./Cliente1/ResourceGroup"
+    source = "./ResourceGroup"
     client_name = local.client_name 
     location = local.location
     client_number = local.client_number
 }
 module "Cliente1_ACR"{
-    source = "./Cliente1/ContainerRegistry"
+    source = "./ContainerRegistry"
     client_name = local.client_name
     location = local.location
     client_number = local.client_number
