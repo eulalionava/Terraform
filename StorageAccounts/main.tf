3c196134fdd6f9dc23c011f4319ecc49541ae7cd
Jesus Hernández Jiménez <jesush@readymind.ms>
2024-05-30 23:07:15 +0000

    Updated main.tf

--- StorageAccounts/main.tf
+++ StorageAccounts/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
+  name                     = "storageaccountname2"
   resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
