8d4123f0b8a82edb93fb116258f61a7355ff28e8
Jesus Hernández Jiménez <jesush@readymind.ms>
2024-05-30 22:24:45 +0000

    Updated main.tf

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -2,10 +2,10 @@ resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 
   subnet {
     name           = "aks-subnet"
-    address_prefix = "10.0.3.0/24"
+    address_prefix = "10.1.3.0/24"
   }
 }
