3746497f812ff8b9094ad2c28e08cea2e105d2e1
Jesus Hernández Jiménez <jesush@readymind.ms>
2024-05-30 20:02:49 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,10 @@ module "KeyVault"{
     key_vault_manager = local.key_vault_manager
     tenant = local.tenant
 }
-
+module "Vnet"{
+    source = "./Vnet"
+    location = local.location
+    resource_group_name = module.resource_group_name.rg_aks_name_out
+}
 #Crear vm para agente
 #Crear vm para grafana y prometheus
