b61723b814475328be107cac8197b5364c892921
Jesus Hernández Jiménez <jesush@readymind.ms>
2024-05-31 21:52:17 +0000

    Updated main.tf

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -8,6 +8,6 @@ resource "azurerm_virtual_network" "example" {
 resource "azurerm_subnet" "example" {
   name                 = "aks-subnet"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = "azurerm_virtual_network"
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.1.3.0/24"]
 }
