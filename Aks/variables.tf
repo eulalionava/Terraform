0ba53394ee61d2be69ac2370febfea4158827407
Jesus Hernández Jiménez <jesush@readymind.ms>
2024-05-31 19:48:42 +0000

    Updated variables.tf

--- Aks/variables.tf
+++ Aks/variables.tf
@@ -6,3 +6,7 @@ variable "resource_group_name" {
     description = "The name of the resource group where the container registry will be created."
     type        = string
 }
+variable "vnet_subnet_id"{
+    type = string
+    description = "The ID of the subnet where the AKS cluster node pool will be located."
+}
