c4532f2245c621851151a3d5977d6dc827316558
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-05 17:07:12 +0000

    Updated main.tf

--- Aks/main.tf
+++ Aks/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_private_dns_zone" "dns" {
-  name                = "privatelink.eastus.azmk8s.io"
+  name                = "privatelink.terraform.eastus.azmk8s.io"
   resource_group_name = var.resource_group_name
 }
 
