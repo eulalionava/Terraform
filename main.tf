f98b163cf1fe14166a43e74b8b214c0207727e5f
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-11 22:47:54 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -29,13 +29,12 @@ module "StorageAccount"{
     source = "./StorageAccounts"
     location = local.location
     resource_group_name = module.ResourceGroup.rg_aks_name_out
-}
+}*/
 module "Aks"{
     source = "./Aks"
     location = local.location
     resource_group_name = module.ResourceGroup.rg_aks_name_out
     vnet_subnet_id = module.Vnet.subnet_id_out
 }
-*/
 #Crear vm para agente
 #Crear vm para grafana y prometheus
