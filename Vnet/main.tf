0ed61feeb4c23e68a0aaac9fb0b212f94fc4ec55
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-11 22:37:02 +0000

    Updated main.tf

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -25,3 +25,9 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.global.name
   address_prefixes     = ["10.120.0.0/24"]
 }
+resource "azurerm_subnet" "subnet-aks" {
+  name                 = "akssubnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.k8.name
+  address_prefixes     = ["10.140.0.0/20"]
+}
