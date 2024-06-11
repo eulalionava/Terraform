775924f557f3447fd1cb605832803c5a9a241c51
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-11 22:49:46 +0000

    Added variables.tf

---
+++ Private_endpoint/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+    type = string
+    description = "localizacion de los recursos"
+}
+
+variable "resource_group_name" {
+    description = "The name of the resource group where the container registry will be created."
+    type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet for the private endpoint."
+  type        = string
+}
+
+variable "acr_id" {
+  description = "The ID of the container registry."
+  type        = string
+}
