27022dacafa41a5046c306763afa8a1b6a4898ae
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-11 22:37:26 +0000

    Deleted Subnet

--- Subnet/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "global" {
-  name                = "SubnetGlobal1"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  address_space       = ["10.120.0.0/24"]
-}
