edac74a09765d871e1d9abfa1593b1166c922cd7
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-11 16:46:04 +0000

    Updated main.tf

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -1,19 +1,25 @@
 resource "azurerm_virtual_network" "global" {
-  name                = "example-network"
+  name                = "global"
   location            = var.location
   resource_group_name = var.resource_group_name
   address_space       = ["10.120.0.0/24"]
 }
-resource "azurerm_virtual_network" "global" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "Api" {
+  name                = "api"
   location            = var.location
   resource_group_name = var.resource_group_name
-  address_space       = ["10.120.0.0/24"]
+  address_space       = ["10.130.0.0/24"]
 }
-
+resource "azurerm_virtual_network" "k8" {
+  name                = "k8"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = ["10.140.0.0/16"]
+}
+/*
 resource "azurerm_subnet" "example" {
   name                 = "aks-subnet2"
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.1.4.0/24"]
-}
+} */
