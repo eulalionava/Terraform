91ea415c5631a11270c4e9b167665fbef6ab1544
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-11 19:44:45 +0000

    Updated main.tf

--- ContainerRegistry/main.tf
+++ ContainerRegistry/main.tf
@@ -2,7 +2,9 @@ resource "azurerm_container_registry" "acr" {
   name                = "crpefprd${var.location}"
   resource_group_name = var.resource_group_name
   location            = var.location
-  sku                 = "Basic"
+  sku                 = "Premium"
   admin_enabled       = true
+  public_network_access_enabled = false
+  
   
 }
