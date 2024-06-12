fbe747e2131213478a7fe56fd0477afbc1cf797a
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-12 17:22:59 +0000

    Updated main.tf

--- Vnet/main.tf
+++ Vnet/main.tf
@@ -49,3 +49,15 @@ resource "azurerm_subnet" "subnet-devops1" {
   virtual_network_name = azurerm_virtual_network.devops.name
   address_prefixes     = ["10.50.0.32/27"]
 }
+
+resource "azurerm_network_interface" "network_interface" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
