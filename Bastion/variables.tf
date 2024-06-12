646ef85cf092c12fa63732104daf6a8a94664aac
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-12 17:35:26 +0000

    Added variables.tf

---
+++ Bastion/variables.tf
@@ -0,0 +1,8 @@
+variable "location" {
+    type = string
+    description = "localizacion de los recursos"
+}
+variable "resource_group_name" {
+    description = "The name of the resource group where the container registry will be created."
+    type        = string
+}
