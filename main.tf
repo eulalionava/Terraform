7a01d506bd7942d5a524e4d8c204ed1187d838b7
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-12 20:02:27 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ module "Bastion"{
     source = "./Bastion"
     location = local.location
     resource_group_name = module.ResourceGroup.rg_hub_name_out
-    vnet_subnet_id = module.Vnet.subnet_id_devops0_out
+    vnet_subnet_id = module.Vnet.subnet_id_devops2_out
 }
 #Crear vm para agente
 #Crear vm para grafana y prometheus
