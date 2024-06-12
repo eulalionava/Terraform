ce4e7c1f4cf19d286023f1591f0ec05a7604b5c7
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-12 03:45:08 +0000

    Updated outputs.tf

--- Vnet/outputs.tf
+++ Vnet/outputs.tf
@@ -1,7 +1,13 @@
-output "azurerm_subnet_id" {
-  value = azurerm_subnet.subnet.id
-}
+
 
 output "subnet_id_pool_out" {
   value = azurerm_subnet.subnet-aks.id
+}
+
+output "azurerm_virtual_network_id" {
+  value = azurerm_virtual_network.k8.id
+}
+
+output "azurerm_subnet_id" {
+  value = azurerm_subnet.subnet.id
 }
