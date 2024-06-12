4b52a296a3d43843e977e020e0c79f4410ae9d39
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-12 14:49:12 +0000

    Updated variables.tf

--- Vnet/variables.tf
+++ Vnet/variables.tf
@@ -6,3 +6,7 @@ variable "resource_group_name" {
     description = "The name of the resource group where the container registry will be created."
     type        = string
 }
+variable "resource_group_name_ntw" {
+    description = "The name of the resource group where the container registry will be created."
+    type        = string
+}
