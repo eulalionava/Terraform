ec202e11fbc6735e5095a8708720814f7d1cec37
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-12 18:38:37 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -74,6 +74,7 @@ module "Bastion"{
     source = "./Bastion"
     location = local.location
     resource_group_name = module.ResourceGroup.rg_aks_name_out
+    vnet_subnet_id = module.Vnet.subnet_id_devops0_out
 }
 #Crear vm para agente
 #Crear vm para grafana y prometheus
