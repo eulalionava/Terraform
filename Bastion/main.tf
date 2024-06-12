f1b10a68a4a947ec69fc2b6f55d2ccf7cd22b428
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-12 17:34:53 +0000

    Added main.tf

---
+++ Bastion/main.tf
@@ -0,0 +1,19 @@
+resource "azurerm_public_ip" "ip" {
+  name                = "pe-stpefprdeastus-001"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "example" {
+  name                = "bas-pef-prd-eastus-001"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.example.id
+    public_ip_address_id = azurerm_public_ip.ip.id
+  }
+}
