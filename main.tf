1a78c86c08a828c263fd33835983f2a129f1328b
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-12 03:37:04 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ module "Private_endpoint" {
   resource_group_name           = module.ResourceGroup.rg_aks_name_out
   subnet_id                     = module.Vnet.azurerm_subnet_id
   private_service_connection_name = "private-service-connection"
-  acr_id                        = module.ContainerRegistry.azurerm_container_registry_id
-  virtual_network_id            = module.network.azurerm_virtual_network_id
+  acr_id                        = module.ContainerRegistry.acr_id
+  virtual_network_id            = module.Vnet.azurerm_virtual_network_id
   acr_name                      = var.acr_name
   private_ip_addresses          = ["10.0.1.4"]
 }
