0e15d2f0de0e876482a1d4a619b3e4eda08deaa3
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-12 17:05:01 +0000

    Updated main.tf

--- Private_endpoint/main.tf
+++ Private_endpoint/main.tf
@@ -11,3 +11,15 @@ resource "azurerm_private_endpoint" "pe" {
     subresource_names              = ["registry"]
   }
 }
+
+resource "azurerm_private_dns_zone" "dns_zone_acr" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "vnl_acr" {
+  name                  = "dns-link-global"
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone_acr.name
+  virtual_network_id    = var.virtual_network_id
+}
