364e171ca8db05d989e8bd008eb21fe45dbba8b2
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-12 17:53:00 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -33,6 +33,13 @@ module "VM"{
     resource_group_name = module.ResourceGroup.rg_ntw_name_out
     vnet_subnet_id = module.Vnet.subnet_id_devops0_out
 }
+
+module "vmwindows" {
+  source                 = "./VMWindows"
+  location               = local.location
+  resource_group_name    = module.ResourceGroup.rg_aks_name_out
+  network_interface_id   = module.network.azurerm_network_interface_id
+}
 /*
 # Llamar al módulo de Private Endpoint
 module "Private_endpoint" {
