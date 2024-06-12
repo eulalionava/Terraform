a449bf951cd15f88406a75d679691f022b78e5f2
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-12 17:49:36 +0000

    Added variables.tf

---
+++ VMWindows/variables.tf
@@ -0,0 +1,12 @@
+variable "location" {
+    type = string
+    description = "localizacion de los recursos"
+}
+variable "resource_group_name" {
+    description = "The name of the resource group where the container registry will be created."
+    type        = string
+}
+variable "vnet_subnet_id"{
+    type = string
+    description = "The ID of the subnet where the AKS cluster node pool system will be located."
+}
