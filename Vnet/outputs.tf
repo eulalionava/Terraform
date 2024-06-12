761e32ef98b89ff25c9a1d883ab437712329044a
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-12 04:06:27 +0000

    Updated outputs.tf

--- Vnet/outputs.tf
+++ Vnet/outputs.tf
@@ -1,13 +1,11 @@
-
-
 output "subnet_id_pool_out" {
   value = azurerm_subnet.subnet-aks.id
 }
 
-output "azurerm_virtual_network_id" {
-  value = azurerm_virtual_network.k8.id
-}
+#output "azurerm_virtual_network_id" {
+#  value = azurerm_virtual_network.k8.id
+#}
 
-output "azurerm_subnet_id" {
+output "subnet_id" {
   value = azurerm_subnet.subnet.id
 }
