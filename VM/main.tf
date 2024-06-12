5e83113d55c3e18d7aba09809a9dfd4e1221000b
Aline Cardenas Sanchez <acardenas@readymind.ms>
2024-06-12 05:28:59 +0000

    Updated main.tf

--- VM/main.tf
+++ VM/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_interface" "network_interface" {
+resource "azurerm_network_interface" "network-interface" {
   name                = "nic-vm-win-pef-devops-0"
   location            = var.location
   resource_group_name = var.resource_group_name
