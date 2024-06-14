b0e4247ffff3684fc202afc451ae7399eeedd354
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-14 00:16:02 +0000

    Updated main.tf

--- AppRegistration/main.tf
+++ AppRegistration/main.tf
@@ -1,17 +1,17 @@
-resource "azuread_application" "example" {
-  name                       = var.app_name
+resource "azuread_application" "app" {
+  name                       = "appcs-pef-prd-eastus-001"
   homepage                   = var.homepage
   identifier_uris            = var.identifier_uris
   reply_urls                 = var.reply_urls
   available_to_other_tenants = false
 }
 
-resource "azuread_service_principal" "example" {
-  application_id = azuread_application.example.application_id
+resource "azuread_service_principal" "app" {
+  application_id = azuread_application.app.application_id
 }
 
-resource "azuread_service_principal_password" "example" {
-  service_principal_id = azuread_service_principal.example.id
+resource "azuread_service_principal_password" "app" {
+  service_principal_id = azuread_service_principal.app.id
   value                = var.client_secret
   end_date             = var.client_secret_end_date
 }
