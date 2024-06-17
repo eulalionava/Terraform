bb5f1a9d8e157c6306aab37250cc4c223cbfd537
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-17 22:36:28 +0000

    Added variables.tf

---
+++ AplicationGateway/variables.tf
@@ -0,0 +1,54 @@
+variable "name" {
+  description = "The name of the Application Gateway"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the Application Gateway"
+  type        = string
+}
+
+variable "sku_name" {
+  description = "The SKU name of the Application Gateway"
+  type        = string
+}
+
+variable "sku_tier" {
+  description = "The SKU tier of the Application Gateway"
+  type        = string
+}
+
+variable "capacity" {
+  description = "The capacity of the Application Gateway"
+  type        = number
+}
+
+variable "subnet_id" {
+  description = "The subnet ID for the Application Gateway"
+  type        = string
+}
+
+variable "frontend_port" {
+  description = "The frontend port for the Application Gateway"
+  type        = number
+}
+
+variable "public_ip_address_id" {
+  description = "The public IP address ID for the Application Gateway"
+  type        = string
+}
+
+variable "backend_ip_address" {
+  description = "The backend IP address for the Application Gateway"
+  type        = string
+}
+
+variable "backend_port" {
+  description = "The backend port for the Application Gateway"
+  type        = number
+}
