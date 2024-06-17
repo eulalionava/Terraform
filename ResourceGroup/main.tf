a6e97f6ffa94291003db19065f4780315746ee6a
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-17 22:40:29 +0000

    Updated main.tf

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -2,19 +2,20 @@ resource "azurerm_resource_group" "rg_hub" {
   name     = "rsg-pef-hub2-prd-${var.location}-001"
   location = var.location
 }
-resource "azurerm_resource_group" "rg_pef" {
-  name     = "rsg-pef-apm-prd-${var.location}-001"
-  location = var.location
-}
-resource "azurerm_resource_group" "rg_aks" {
-  name     = "rsg-pef-k8s-prd-${var.location}-001"
-  location = var.location
-}
-resource "azurerm_resource_group" "rg_cdn" {
-  name     = "rsg-pef-k8s-cdn-prd-${var.location}-001"
-  location = var.location
-}
-resource "azurerm_resource_group" "rg_ntw" {
-  name     = "rsg-pef-k8s-ntw-prd-${var.location}-001"
-  location = var.location
-}
+
+// resource "azurerm_resource_group" "rg_pef" {
+//   name     = "rsg-pef-apm-prd-${var.location}-001"
+//   location = var.location
+// }
+// resource "azurerm_resource_group" "rg_aks" {
+//   name     = "rsg-pef-k8s-prd-${var.location}-001"
+//   location = var.location
+// }
+// resource "azurerm_resource_group" "rg_cdn" {
+//   name     = "rsg-pef-k8s-cdn-prd-${var.location}-001"
+//   location = var.location
+// }
+// resource "azurerm_resource_group" "rg_ntw" {
+//   name     = "rsg-pef-k8s-ntw-prd-${var.location}-001"
+//   location = var.location
+// }
