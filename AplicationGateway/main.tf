d9a33fabe453f6f1f57f72fb12b8805a05c7dbbf
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-17 23:34:12 +0000

    Updated main.tf

--- AplicationGateway/main.tf
+++ AplicationGateway/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "public_ip" {
   name                = "public_ip"
-  resource_group_name = azurerm_resource_group.natus-seg-rg.name
-  location            = azurerm_resource_group.natus-seg-rg.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   allocation_method   = "Static"
   sku                 = "Standard"
 
