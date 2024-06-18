30c5aa046f7ab98d2b854e6a48b2264beaffa391
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-18 03:25:47 +0000

    Updated main.tf

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -7,15 +7,15 @@ resource "azurerm_resource_group" "rg_hub" {
 //   name     = "rsg-pef-apm-prd-${var.location}-001"
 //   location = var.location
 // }
-// resource "azurerm_resource_group" "rg_aks" {
-//   name     = "rsg-pef-k8s-prd-${var.location}-001"
-//   location = var.location
-// }
+resource "azurerm_resource_group" "rg_aks" {
+  name     = "rsg-pef-k8s-prd-${var.location}-001"
+  location = var.location
+}
 // resource "azurerm_resource_group" "rg_cdn" {
 //   name     = "rsg-pef-k8s-cdn-prd-${var.location}-001"
 //   location = var.location
 // }
-// resource "azurerm_resource_group" "rg_ntw" {
-//   name     = "rsg-pef-k8s-ntw-prd-${var.location}-001"
-//   location = var.location
-// }
+resource "azurerm_resource_group" "rg_ntw" {
+  name     = "rsg-pef-k8s-ntw-prd-${var.location}-001"
+  location = var.location
+}
