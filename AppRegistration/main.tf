f4c1c3115f07884675d39b02f4198a3b46b3efd5
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-18 05:44:04 +0000

    Updated main.tf

--- AppRegistration/main.tf
+++ AppRegistration/main.tf
@@ -2,6 +2,25 @@ resource "azuread_application" "app" {
   display_name               = "appcs-pef-prd-eastus-001"
   identifier_uris            = var.identifier_uris
   available_to_other_tenants = false
+
+  web {
+    home_page_url            = var.home_page_url
+    redirect_uris            = var.redirect_uris
+    logout_url               = var.logout_url
+    implicit_grant {
+      access_token_issuance_enabled = var.access_token_issuance_enabled
+      id_token_issuance_enabled     = var.id_token_issuance_enabled
+    }
+  }
+
+  required_resource_access {
+    resource_app_id = var.resource_app_id
+
+    resource_access {
+      id   = var.resource_access_id
+      type = "Scope"
+    }
+  }
 }
 
 resource "azuread_service_principal" "app" {
