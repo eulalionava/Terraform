c6b229cfe080fded73e82068ed260dc442105cd5
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-18 03:25:05 +0000

    Updated outputs.tf

--- ResourceGroup/outputs.tf
+++ ResourceGroup/outputs.tf
@@ -6,15 +6,15 @@ output "rg_hub_name_out"{
 //     value = azurerm_resource_group.rg_pef.name
 
 // }
-// output "rg_aks_name_out"{
-//     value = azurerm_resource_group.rg_aks.name
+output "rg_aks_name_out"{
+    value = azurerm_resource_group.rg_aks.name
 
-// }
+}
 // output "rg_cdn_name_out"{
 //     value = azurerm_resource_group.rg_cdn.name
 
 // }
-// output "rg_ntw_name_out"{
-//     value = azurerm_resource_group.rg_ntw.name
+output "rg_ntw_name_out"{
+    value = azurerm_resource_group.rg_ntw.name
 
-// }
+}
