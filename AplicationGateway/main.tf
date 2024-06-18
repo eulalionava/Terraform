5b0079f53ab29d26f727156013320e2e826157e6
Eulalio Cruz Nava <eulalio@readymind.ms>
2024-06-18 02:21:29 +0000

    Updated main.tf

--- AplicationGateway/main.tf
+++ AplicationGateway/main.tf
@@ -30,6 +30,7 @@ resource "azurerm_application_gateway" "appgw" {
   resource_group_name = var.resource_group_name
   location            = var.location
   public_ip_address_id = azurerm_public_ip.public_ip.id
+  backend_ip_address = "10.140.1.0"
 
   tags = {
     Environment = "Develop"
